ff34f8f61ed1a22ea683e15712098f2c95c201c2
ErwinHafiz <101485637+ErwinHafiz@users.noreply.github.com>
2024-07-07 19:07:21 +0700

    Update Secrets.h

--- Secrets.h
+++ Secrets.h
@@ -1,11 +1,11 @@
 #include <pgmspace.h>
  
 #define SECRET
-#define THINGNAME "ESP32_LDR2"                         //change this
+#define THINGNAME "ESP32_LDR2"                        
  
-const char WIFI_SSID[] = "alterwin";               //change this
-const char WIFI_PASSWORD[] = "123456788";           //change this
-const char AWS_IOT_ENDPOINT[] = "ac4v4pkzew2n-ats.iot.ap-southeast-1.amazonaws.com";       //change this
+const char WIFI_SSID[] = "alterwin";              
+const char WIFI_PASSWORD[] = "123456788";       
+const char AWS_IOT_ENDPOINT[] = "ac4v4pkzew2n-ats.iot.ap-southeast-1.amazonaws.com";     
  
 // Amazon Root CA 1
 static const char AWS_CERT_CA[] PROGMEM = R"EOF(
@@ -14,7 +14,7 @@ HERE CERTIFICATE CA
 -----END CERTIFICATE-----
 )EOF";
  
-// Device Certificate                                               //change this
+// Device Certificate                                             
 static const char AWS_CERT_CRT[] PROGMEM = R"KEY(
 -----BEGIN CERTIFICATE-----
 HERE CERTIFICATE CRT
@@ -23,7 +23,7 @@ HERE CERTIFICATE CRT
  
 )KEY";
  
-// Device Private Key                                               //change this
+// Device Private Key                                     
 static const char AWS_CERT_PRIVATE[] PROGMEM = R"KEY(
 -----BEGIN RSA PRIVATE KEY-----
 HERE CERTIFICATE PRIVATE
